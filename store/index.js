898a8985c26adbfb2326d3f5b678716ca64d98ee
hjl <jingliqaq@gmail.com>
2020-11-02 18:32:38 +0800

    引入请求库和Vuex

---
+++ store/index.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+import $H from '../common/request.js';
+
+export default new Vuex.Store({
+	state: {
+		user: null,
+		token: null
+	},
+	actions: {
+		// 初始化用户登录状态
+		initUser({
+			state
+		}) {
+			let u = uni.getStorageSync('user')
+			let t = uni.getStorageInfoSync('token')
+			if (u) {
+				state.user = JSON.parse(u)
+				state.token = t
+			}
+		},
+		logout({
+			state
+		}) {
+			$H.post('/logout', {},{
+				token: true,
+				toast: false
+			})
+			state.user = null
+			state.token = null
+			uni.removeStorageSync('user')
+			uni.removeStorageSync('token')
+		},
+		login({ state }, user) {
+			state.user = user
+			state.token = user.token
+			// 将登陆用户信心和token存入缓存
+			uni.setStorageSync('user', JSON.stringify(user))
+			uni.setStorageSync('token', user.token)
+		},
+		getUserInfo({
+			state
+		}) {
+			$H.get('/user/info', {
+				token: true,
+				noJump: true,
+				toast: false
+			}).then(res => {
+				state.user = res
+				uni.setStorage({
+					key: 'user',
+					data: JSON.stringify(state.user)
+				})
+			})
+		}
+	}
+})
