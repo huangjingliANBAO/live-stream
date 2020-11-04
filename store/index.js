0f407096a5fb26a5e87136fbab6af9809724a9f4
hjl <jingliqaq@gmail.com>
2020-11-04 22:38:53 +0800

    离开直播间

--- store/index.js
+++ store/index.js
@@ -1,80 +1,96 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
-
 import $H from '../common/request.js';
 import $C from '../common/config.js';
-import io from '../common/uni-socket-io.js';
+import io from '../common/uni-socket.io.js';
+Vue.use(Vuex)
+
 
 export default new Vuex.Store({
 	state: {
 		user: null,
 		token: null,
-		socket:null
+		socket: null
 	},
 	actions: {
 		//连接socket
-		connectSocket({
-			state,
-			dispatch
-		}){
-			const S = io($C.socketUrl,{
-				query:{},
-				transports:['websocket'],
-				timeout:5000
-			})
-			//全局事件，用来监听在线人数
-			let onlineEvent = (e)=>{
-				uni.$emit('live',{
-					type:"online",
-					data:e
-				})
-			}
-			//监听连接
-			S.on('connect',() =>{
-				console.log('已连接')
-				state.socket = S
-				//socket.io唯一链接id,可以监控这个id实现点对点通讯
-				const{
-					id
-				} = S
-				S.on(S.id,(e) => {
-					let d = e.data
-					if(d.action === 'error'){
-						let msg = d.payload
-						return uni.showToast({
-							title:msg,
-							icon:'none'
-						});
-					}
-				})
-				//监听在线用户信息
-				S.on('online',onlineEvent)
-			})
-			//监听失败
-			S.on('error',() => {
-				console.log('连接失败')
-			})
-			//监听断开
-			S.on('disconnect',() => {
-				console.log('已断开')
-			})
-		},
-		//需要登录才能访问的方法，这个只能放在Vuex里才能生效
+		  connectSocket({
+		   state,
+		   dispatch
+		  }){
+			console.log(12121212)
+		   const S =io($C.sorketUrl,{
+		    query:{},
+		    transports:['websocket'],
+		    timeout:5000
+		   })
+		   // 全局事件，用来监听在线人数
+		   let onlineEvent = (e) => {
+			   uni.$emit('live', {
+				   type: 'online',
+				   data: e
+			   })
+		   }
+		   //监听连接
+		     S.on('connect',()=>{
+		      console.log('socket已连接')
+			  // S.emit('test','测试socket')
+			  state.socket = S
+			  // socket.io 唯一链接id，可以监控这个id实现对点通讯
+			  const {
+				  id
+			  } = S
+			  S.on(id, (e) => {
+				  let d = e.data
+				  if(d.action === 'error') {
+					  let msg = d.payload
+					  return uni.showToast({
+						  title: msg,
+						  icon: 'none'
+					  });
+				  }
+			  })
+			  // 监听在线用户信息
+			  S.on('online', onlineEvent)
+			  // 监听来自服务器端的消息
+			  S.on(S.id, (e) => {
+				  console.log(e)
+			  })
+		     })
+			  // 移除监听事件
+			  const removeListener = () => {
+				  if(S) {
+					  S.removeListener('online', onlineEvent)
+				  }
+			  }
+		     //监听失败
+		     S.on('error',()=>{
+			  removeListener()
+			  state.socket = null
+		      console.log('socket连接失败')
+		     })
+		     //监听断开
+		     S.on('disconnect',()=>{
+				 removeListener()
+				 state.socket = null
+		      console.log('已断开')
+		     })
+		  },
+		// 需要登陆才能访问的方法，这个只能放在Vuex里才能生效
 		authMethod({
 			state
-		},callback){
-			if(!state.token){
+		},callback) {
+			if(!state.token) {
 				uni.showToast({
-					title:'请先登录',
-					icon:'none'
+					title: '请先登录',
+					icon: 'none'
 				});
 				return uni.navigateTo({
-					url:'/pages/login/login'
+					url: '/pages/login/login'
 				});
-				callback()
 			}
+			callback()
 		},
 		// 初始化用户登录状态
 		initUser({
